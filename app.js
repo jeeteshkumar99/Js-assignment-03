d115751ab741b7853b7f5f85b06e9de0ead5d740
jeeteshkumar99 <jeeteshkumasuthar2003@gmail.com>
2024-07-17 21:14:36 +0500

    js-assignmnet3

---
+++ app.js
@@ -0,0 +1,104 @@
+// // Declare string array
+// var arr = ["karachi ","Lahore","Rawalpidi","Mithi","Islamabad"]
+
+// // declare number array
+// var num = [1 ,2 ,3 ,4 ,5]
+
+// // mix array
+// var mixed =[2,"a","b","c",3]
+
+// // print available pakistan eduacation
+// var edu = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS","M. Phil.", "PhD"]
+
+// document.write("Qualifications:  <br><br> 1) "+ edu[0] +"<br> 2) "+edu[1] + edu[2] +"<br> 2) "+ edu[3] +"<br> 2) "+ edu[4] +"<br> 2) "+ edu[5] +"<br> 2) "+ edu[6]   )
+
+
+// // top four movies
+// var movies =[ ]
+// document.write("<b> Top Movies of 2015 </b>  <br> <br>" )
+// movies.push("1) KGF <br>" ,"2) RaniGanchh <br>" , "3) 12th Fail <br>" , "4) Lakshmi Bom <br>" )
+
+// document.write(movies)
+// document.write("<b> Length of array </b> ="+ movies.length)
+
+
+
+// // declare favt cars and get lenght
+// var car = [ "Audi" , "Volvo" ,"Ford" ,"Lamborghini" ]
+//  document.write(car)
+
+// document.write("2nd index of an array : "+ car[1])
+// document.write(" <br> third index of an array : "+ car[2])
+
+
+
+// // write student name and array and store marks
+// var stu = ["Michel","John ","Tony"]
+
+// var marks = [320 ,230, 480]
+
+// // document.write(marks)
+
+// var MichelPerc = marks[0] /500 *100
+// var johnPerc = marks[1] /500 *100
+// var tonyPerc = marks[2] /500 *100
+
+
+// document.write("The score Michel of is "+ marks[0] +". Percentage is  " +MichelPerc +"%" + "<br>")
+// document.write("The score of John is "+ marks[1] +". Percentage is  " +johnPerc +"%" + "<br>")
+// document.write("The score of Tony  is "+ marks[2] +". Percentage is " +tonyPerc +"%") 
+
+
+
+// // write cities name and copy
+// var cities = ["Karachi","Lahore","Islamabad","Queeta","Peshawar"]
+
+// document.write(cities +"<br>")
+
+// var selectedCites = cities.slice(2,4)
+// document.write("Selected cities list: <br>"+selectedCites)
+
+
+
+// // array join method
+// var arr2 = ["This", " is " , " my ", " cat."]
+// document.write(arr2 +"<br>")
+
+// var joinarr = arr2.join("")
+// document.write(joinarr)
+
+
+// // print in fifo
+// var comp =["Keyboard","Mouse","Printer","Moniter"]
+
+// document.write(comp[0] + "<br>")
+// document.write(comp[1] + "<br>")
+// document.write(comp[2] + "<br>")
+// document.write(comp[3] + "<br>")
+
+
+// // Write a program to store phone manufacturers (Apple,
+// //Samsung, Motorola, Nokia, Sony & Haier) in an array. Display
+// //the following dropdown/select menu in your browser using
+// //document.write() method:
+
+// var mobile =["Apple ","Samsung","Motrola","Nokia","Sony"]
+
+//     document.write(
+//         "<select>"+   
+//             "<Option>" +  mobile[0] +"</Option>"  +
+//             "<Option>" +  mobile[1] +"</Option>"  +
+//             "<Option>" +  mobile[2] +"</Option>"  +
+//             "<Option>" +  mobile[3] +"</Option>"  +
+//         "</select>"
+//     )
+    
+
+
+// // Multidimentional array declaration
+// var multiArr = [[1,2,3],[2,"a","b"]]
+// document.write(multiArr) 
+
+
+// var multiDiArr =[[1102], [2101],[1233]]
+// document.write(multiDiArr[0]+"<br>"+multiDiArr[1]+"<br>"+multiDiArr[2])
